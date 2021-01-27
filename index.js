fb681c876aa1cbcae59bd426ae74ff1662d07a37
DeepaNethesh <deepanethesh2011@gmail.com>
2021-01-28 10:39:32 +1100

    javascript examples

---
+++ index.js
@@ -0,0 +1,38 @@
+//adding two numbers
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(addNumbers(4, 7))//functionname(arguments)
+
+console.log("===============")
+// Multiply the number
+function multiplyNumber(num) {
+    return num * num;
+}
+
+console.log(multiplyNumber(5));
+
+console.log("===============")
+
+//say Hello
+
+function sayHello(name) {
+    return 'Hello ' +name;
+}
+
+console.log(sayHello('Anya'))
+
+console.log("===============")
+
+//to convert the string to uppercase
+const toUpper = (string) => {
+    return string.toUpperCase()
+}
+console.log(toUpper("deepa murali"));
+
+let name = 'deepa';
+console.log(name.toUpperCase());
+
+
+
